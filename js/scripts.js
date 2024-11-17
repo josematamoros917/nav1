71a91a9cbf7bc5d7983a11ebf183550b2c633ee9
josematamoros917 <josematamoros917@gmail.com>
2024-11-17 01:52:07 -0300

    cambio por tarjeta canva

--- js/scripts.js
+++
@@ -1,6 +0,0 @@
-// Animar stickers al hacer clic
-document.querySelectorAll('.sticker').forEach(sticker => {
-    sticker.addEventListener('click', () => {
-        alert('¡Este sticker es tu favorito!');
-    });
-});
